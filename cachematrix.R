450b58befe417f7b3894935e2089236d943c02ac
zicarej <john.zicarelli@gmail.com>
2014-06-21 21:56:04 -0700

    Update 6-21-2014
    R Code for functions makeCacheMatrix and cacheSolve. Part of
    Programming Assignment 2 for R Programming Course.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,28 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a square, invertible matrix as an argument and returns a
+## list of four functions.
+##
+## For example, x<-makeCacheMatrix() returns:
+##    x$get(x) returns x
+##    x$getinverse(x) returns x inverse
+##    x$set(x) archives x
+##    x$setinverse(x) archives x inverse
+##
 
 makeCacheMatrix <- function(x = matrix()) {
+    m<-NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+    setinverse = setinverse,
+    getinverse = getinverse)
 
 }
 
@@ -12,4 +31,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse
+        if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+        }
+        data <- x$get
+        m <- solve(data, ...)
+        x$setinverse(m)
 }
